2d0a33e5762a791476ac38ada92b99b1c41bd819
Nazar <nazaranddobriy@gmail.com>
2023-11-24 15:24:56 +0200

    feat: add header-currency template

--- src/core/components/header/header.component.ts
+++ src/core/components/header/header.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
+import { CURRENCY } from 'src/core/consts/currency';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  CURRENCY = CURRENCY;
+}
