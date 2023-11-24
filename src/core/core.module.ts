7978d79e0432cc8f43ee1007967d1dba9d1cfeb1
Nazar <nazaranddobriy@gmail.com>
2023-11-24 15:25:21 +0200

    fix: connent components and modules to core module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from 'src/shared/shared.module';
 import { HeaderComponent } from './components/header/header.component';
+import { HeaderCurrencyComponent } from './components/header/header-currency/header-currency.component';
+import { CurrencyInputDirective } from './directives/currency-input.directive';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    HeaderCurrencyComponent,
+    CurrencyInputDirective
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    SharedModule
   ],
   exports: [
     HeaderComponent
